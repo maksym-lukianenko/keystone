f21fae4c83a799bac4fe96be6c3077b4a639f810
Maksym Lukianenko <maximkharkow@gmail.com>
2020-01-30 16:59:32 +0200

    Imrovements

--- admin/client/App/components/Footer/index.js
+++ admin/client/App/components/Footer/index.js
@@ -46,6 +46,16 @@ var Footer = React.createClass({
 					>
 						{brand + (appversion ? (' ' + appversion) : '')}
 					</a>
+					<span> powered by </span>
+					<a
+						href="http://keystonejs.com"
+						target="_blank"
+						className={css(classes.link)}
+						tabIndex="-1"
+					>
+						KeystoneJS
+					</a>
+					<span> version {version}.</span>
 					{this.renderUser()}
 				</Container>
 			</footer>
--- admin/client/App/screens/Item/actions.js
+++ admin/client/App/screens/Item/actions.js
@@ -136,7 +136,7 @@ export function deleteItem (id, router) {
 			}
 			// TODO Proper error handling
 			if (err) {
-				console.error(err.error || 'Error deleting item, please try again!');
+				alert(err.error || 'Error deleting item, please try again!');
 			} else {
 				dispatch(loadItems());
 			}
